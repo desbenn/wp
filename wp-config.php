9e88927257e75b02c9c1704119056e09d01b5aa2
desbenn <desbenn@adassa>
2026-09-25 18:20:58 -0500

    General updates.

--- wp-config.php
+++ wp-config.php
@@ -87,7 +87,7 @@ $table_prefix = 'wp_';
  */
 define('WP_DEBUG', false);
 
-/* Add any custom values between this line and the "stop editing" line... */
+/* Add any custom values between this line and the "stop editing" line. */
 
 define( 'WP_DEBUG', true ); 
 
